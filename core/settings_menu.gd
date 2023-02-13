b9e5bc167420351fa9b87c952973224208e14144
William Edwards <shadowapex@gmail.com>
2023-02-12 16:57:29 -0800

    Use OGUI components for settings

--- core/settings_menu.gd
+++ core/settings_menu.gd
@@ -1,13 +1,15 @@
 extends Control
 
 var SettingsManager := load("res://core/global/settings_manager.tres") as SettingsManager
-@onready var text_edit: LineEdit = $VBoxContainer/HBoxContainer/LineEdit
+@onready var text_edit: ComponentTextInput = $%TextInput
 @onready var _api_key: String = SettingsManager.get_value("plugin.steamgriddb", "api_key", "")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text_edit.text = _api_key
+	text_edit.text_submitted.connect(_on_line_edit_text_submitted)
+	text_edit.focus_exited.connect(_on_line_edit_focus_exited)
 
 
 func _on_line_edit_focus_exited() -> void:
